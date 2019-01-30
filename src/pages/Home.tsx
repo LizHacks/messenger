2033c73da5cdda2bc81f9eb5d9f9b351e01d81f3
Peter East <petereast@pm.me>
2019-01-30 15:21:17 +0000

    use new stuff in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,29 @@
 import * as React from 'react';
+import getActiveConversation from '../selectors/get_active_conversation';
+import { AppState } from '../reducers';
+import { connect } from 'react-redux';
 import { Navbar, Columns, Button, Level, Hero} from 'react-bulma-components';
+import {UserDetail, Conversation, MessageDetail } from '../types';
+
+import { switch_chat } from '../actions/conversations';
 
 import ChatsList from '../components/ChatsList';
 import ChatViewer from '../components/ChatViewer';
 
+const example_user = {
+  user_id: "some-user-id",
+  name: "Bobby Beans",
+  organisation_id: "some-org-id",
+};
 // import RepositiveLogo from '../../assets/repositive_logo.svg';
+// tslint:disable-next-line
 const large = require('../../assets/repositive_logo.svg');
 
-export default class Home extends React.PureComponent<any, any> {
-  public render() {
+class Home extends React.PureComponent<any, any> {
+  public render = () => {
     return (
       <div>
-        <Navbar size="small" className="is-marginless">
+        <Navbar size="small" className="is-fixed-top">
           <Navbar.Brand>
             <Navbar.Item>
               <img src={large} alt="Repositive" height="100%"/>
@@ -21,17 +33,23 @@ export default class Home extends React.PureComponent<any, any> {
             </Navbar.Item>
           </Navbar.Brand>
         </Navbar>
-        <Columns>
+        <Columns style={{marginTop: "30px"}}>
           <Columns.Column className="is-one-quarter">
-            <ChatsList chat_names = {
-              ['Bobby Beans', 'Richard Head', 'Steve Warr']
-            }/>
+            <ChatsList
+              switchAction={(id: string) => this.props.dispatch(switch_chat(id))}
+              conversations={this.props.conversations.data}
+              active_id={this.props.active_conversation.conversation_id}
+            />
           </Columns.Column>
-          <Columns.Column>
-            <ChatViewer />
+          <Columns.Column className="is-three-quarters">
+            <ChatViewer conversation={this.props.active_conversation} me={example_user}/>
           </Columns.Column>
         </Columns>
       </div>
     );
   }
 }
+
+export default connect(
+  (s: AppState) => ({conversations: s.conversations, active_conversation: getActiveConversation(s) }),
+)(Home);
