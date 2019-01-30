85f354acc315c2453df15bf27c947854b0ddb490
Peter East <petereast@pm.me>
2019-01-30 15:21:07 +0000

    Add some global types

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+
+export interface OrganisationDetail {
+  is_vendor: boolean;
+  is_consumer: boolean;
+  is_repositive: boolean;
+
+  organisation_name: string; // from orgs domain
+  organisation_type: string; // used to compute above booleans
+  organisation_id: string; // UUID
+}
+
+export interface UserDetail {
+  name: string; // Comes from the accounts domain
+  user_id: string; // Comes from the organisations domain
+  organisation_id: string;
+  avatar_url?: string;
+}
+
+export interface MessageDetail { // Aggregated by message sent events
+  from: UserDetail; // user_id
+  to: UserDetail; // user_id
+  message: string;
+  time: string; // datetime
+}
+
+export interface Conversation {
+  active: boolean;
+  conversation_id: string; // UUID used to aggregate conversation threads
+  topic: string;
+  members: UserDetail[]; // Get other members filter(user => user.user_id != me.user_id)
+  messages: MessageDetail[];
+}
