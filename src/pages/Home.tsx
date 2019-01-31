0af5b40cff2e775fd65aa434894a08f03e67f8b1
Peter East <petereast@pm.me>
2019-01-31 12:47:21 +0000

    Add polling and conversation details to Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,17 @@ import * as React from 'react';
 import getActiveConversation from '../selectors/get_active_conversation';
 import { AppState } from '../reducers';
 import { connect } from 'react-redux';
-import { Navbar, Columns, Button, Level, Hero} from 'react-bulma-components';
+import { Section, Heading, Navbar, Columns, Button, Level, Hero} from 'react-bulma-components';
 import {UserDetail, Conversation, MessageDetail } from '../types';
 
-import { switch_chat } from '../actions/conversations';
+import { switch_chat, poll_messages, send_message } from '../actions/conversations';
+import { load_from_localstorage } from '../actions/me';
 
 import ChatsList from '../components/ChatsList';
 import ChatViewer from '../components/ChatViewer';
 
 const example_user = {
-  user_id: "some-user-id",
+  user_id: 'this-is-the-example-user',
   name: "Bobby Beans",
   organisation_id: "some-org-id",
   organisation: {
@@ -24,6 +25,11 @@ const example_user = {
 //              <img src={large} alt="Repositive" height="100%"/>
 
 class Home extends React.PureComponent<any, any> {
+  public componentDidMount = () => {
+    console.log("POLLING!!");
+    setInterval(() => this.props.dispatch(poll_messages()), 1000);
+    this.props.dispatch(load_from_localstorage());
+  }
   public render = () => {
     return (
       <div>
@@ -42,11 +48,26 @@ class Home extends React.PureComponent<any, any> {
             <ChatsList
               switchAction={(id: string) => this.props.dispatch(switch_chat(id))}
               conversations={this.props.conversations.data}
-              active_id={this.props.active_conversation.conversation_id}
+              active_id={(this.props.active_conversation || {}).conversation_id || ''}
             />
           </Columns.Column>
           <Columns.Column className="is-three-quarters">
-            <ChatViewer conversation={this.props.active_conversation} me={example_user as any}/>
+            {(this.props.active_conversation) ? (
+              <ChatViewer
+                conversation={this.props.active_conversation}
+                me={{user_id: this.props.user_id}as any}
+                onSendMessage={() => this.props.dispatch(
+                  send_message(this.props.active_conversation, {user_id: this.props.user_id}as any))}
+              />
+            ) : (<Section>
+                <Hero color="light">
+                  <Hero.Body>
+                    <Heading>You don't have any conversations.</Heading>
+                    <Heading subtitle>Don't be so fucking quiet you nerd</Heading>
+                  </Hero.Body>
+                </Hero>
+                </Section> )
+            }
           </Columns.Column>
         </Columns>
       </div>
@@ -55,5 +76,6 @@ class Home extends React.PureComponent<any, any> {
 }
 
 export default connect(
-  (s: AppState) => ({conversations: s.conversations, active_conversation: getActiveConversation(s) }),
+  (s: AppState) => (
+    {conversations: s.conversations, active_conversation: getActiveConversation(s), user_id: s.me.user_id }),
 )(Home);
