56b71d94ca5fc68a580029775cbdfdabb0e25dc2
Peter East <petereast@pm.me>
2019-02-03 18:10:47 +0000

    Tidy up support

--- src/components/OtherPeopleInThread.tsx
+++ src/components/OtherPeopleInThread.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Conversation, UserDetail } from '../types';
 
 export default ({conversation, me}: {conversation: Conversation, me: UserDetail}) => {
-  console.log({me});
   return (
     <span>{
       conversation.members
