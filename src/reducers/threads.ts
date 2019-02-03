5e1a9d464cb76c4be49be9425e5b854771165ed6
Peter East <petereast@pm.me>
2019-02-03 18:08:25 +0000

    Add topic and members edit functionality

--- src/reducers/threads.ts
+++ src/reducers/threads.ts
@@ -2,13 +2,25 @@
 //
 
 import { ThreadsState } from '.';
-import { SHOW_NEW_THREAD_FORM, HIDE_NEW_THREAD_FORM } from '../actions/threads';
+import {
+  SHOW_NEW_THREAD_FORM,
+  HIDE_NEW_THREAD_FORM,
+  EDIT_NEW_THREAD_TOPIC,
+  EDIT_NEW_THREAD_MEMBERS,
+  CREATE_NEW_THREAD_BEGIN,
+  CREATE_NEW_THREAD_SUCCESS,
+} from '../actions/threads';
 
 const defaultState = {
   new_thread_editor_visible: false,
+  new_thread_topic: '',
+  new_thread_members: '',
+  create_new_thread_button_enabled: false,
 };
 
 export default function threads(state = defaultState, action = {} as any) {
+  const is_valid_input = (st: any) => st.new_thread_topic.length > 0
+    && st.new_thread_members.length > 0;
   switch (action.type) {
     case SHOW_NEW_THREAD_FORM:
       return {
@@ -19,6 +31,25 @@ export default function threads(state = defaultState, action = {} as any) {
       return {
         ...state,
         new_thread_editor_visible: false,
+      };
+    case EDIT_NEW_THREAD_TOPIC:
+      return {
+        ...state,
+        new_thread_topic: action.value,
+        create_new_thread_button_enabled: is_valid_input(state),
+      };
+    case EDIT_NEW_THREAD_MEMBERS:
+      return {
+        ...state,
+        new_thread_members: action.value,
+        create_new_thread_button_enabled: is_valid_input(state),
+      };
+    case CREATE_NEW_THREAD_SUCCESS:
+      return {
+        ...state,
+        new_thread_topic: '',
+        new_thread_members: '',
+        create_new_thread_button_enabled: false,
       };
     default:
       return state;
