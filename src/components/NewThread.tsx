12f821598890726b6baaa0be8e63c409a6c03aa2
Peter East <petereast@pm.me>
2019-02-03 18:09:28 +0000

    Add new actions to NewThread component

--- src/components/NewThread.tsx
+++ src/components/NewThread.tsx
@@ -5,12 +5,30 @@ import { Button, Heading, Box, Form } from 'react-bulma-components';
 
 export default ({
   me,
+  isButtonEnabled,
   hideNewThreadAction,
   createNewThreadAction,
+  edit_thread_topic: {
+    editNewThreadTopic,
+    value: topic_value,
+  },
+  edit_thread_members: {
+    editNewThreadMembers,
+    value: members_value,
+  },
 }: {
   me?: UserDetail,
+  isButtonEnabled: boolean,
   hideNewThreadAction: any,
-  createNewThreadAction?: any,
+  createNewThreadAction: any,
+  edit_thread_topic: {
+    editNewThreadTopic: any,
+    value: string,
+  },
+  edit_thread_members: {
+    editNewThreadMembers: any,
+    value: string,
+  },
 }) => {
   return (
     <Box>
@@ -20,7 +38,7 @@ export default ({
           Topic
         </Form.Label>
         <Form.Control>
-          <Form.Input placeholder="Conversation topic"/>
+          <Form.Input placeholder="Conversation topic" onChange={editNewThreadTopic} value={topic_value}/>
         </Form.Control>
       </Form.Field>
       <Form.Field>
@@ -28,12 +46,12 @@ export default ({
           Participants
         </Form.Label>
         <Form.Control>
-          <Form.Input placeholder="Type some names"/>
+          <Form.Input placeholder="Type some names" onChange={editNewThreadMembers} value={members_value}/>
         </Form.Control>
       </Form.Field>
       <Form.Field kind="group">
         <Form.Control>
-          <Button className='is-primary' onClick={createNewThreadAction}>New Topic</Button>
+          <Button className='is-primary' disabled={!isButtonEnabled} onClick={createNewThreadAction}>New Topic</Button>
         </Form.Control>
         <Form.Control>
           <Button className='is-danger is-outlined' onClick={hideNewThreadAction}>Cancel</Button>
